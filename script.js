194f3d41e26d359d694ffd283e6570ce760057b0
Steven Wallace <steven@thewallaces.ca>
2016-10-14 21:14:48 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,70 @@
+var wsUri = "ws://localhost:9000";
+var output;
+
+function init() {
+  output = document.getElementById("output");
+  initWebSocket();
+}
+
+function initWebSocket() {
+  websocket = new WebSocket(wsUri);
+  websocket.onopen = function(evt) { onOpen(evt) };
+  websocket.onclose = function(evt) { onClose(evt) };
+  websocket.onmessage = function(evt) { onMessage(evt) };
+  websocket.onerror = function(evt) { onError(evt) };
+}
+
+function onOpen(evt) {
+  var elem = document.getElementById(0);
+  elem.innerHTML = "CONNECTED";
+}
+
+function onClose(evt) {
+  var elem = document.getElementById(0);
+  elem.innerHTML = "DISCONNECTED";
+}
+
+function onMessage(evt) {
+  var msg = JSON.parse(evt.data);
+  var id = msg.id;
+  var text = msg.text;
+  writeToScreen(id, text);
+}
+
+function onError(evt) {
+  console.log(evt.data);
+}
+
+function doSend(message) {
+  writeToScreen(1, message);
+  websocket.send(message);
+}
+
+function writeToScreen(id, message) {
+  var elem = document.getElementById(id);
+  if (typeof(elem) == 'undefined' || elem == null) {
+    elem = document.createElement("p");
+    elem.id = id;
+    elem.style.wordWrap = "break-word";
+    elem.style.color = getRandomColour();
+    output.appendChild(elem);
+  }
+  elem.innerHTML = elem.innerHTML + message;
+}
+
+function keyListener(event) {
+  doSend(event.key);
+}
+
+function getRandomColour() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+window.addEventListener("load", init, false);
+
+window,addEventListener("keypress", keyListener, false);
---
+++ server.py
@@ -0,0 +1,50 @@
+from autobahn.asyncio.websocket import WebSocketServerProtocol
+from uuid import uuid4
+import json
+
+connected = set()
+
+class ServerProtocol(WebSocketServerProtocol):
+
+    def onConnect(self, request):
+        global connected
+        connected.add(self)
+        self.id = uuid4()
+        print("Connection from: {}".format(request.peer))
+        print(self)
+
+    def onClose(self, wasClean, code, reason):
+        global connected
+        connected.remove(self)
+        print("Connection close: {}".format(reason))
+
+    def onMessage(self, payload, isBinary):
+        global connected
+        print(self.id.hex, 'sent', payload)
+        text = payload.decode('utf8')
+        new_payload = {'id': self.id.hex, 'text': text}
+        for socket in connected:
+            if socket is not self:
+                socket.sendMessage(json.dumps(new_payload, ensure_ascii = False).encode('utf8'))
+                #socket.sendMessage(payload, isBinary)
+        #self.sendMessage(payload, isBinary)
+
+if __name__ == '__main__':
+
+    import asyncio
+
+    from autobahn.asyncio.websocket import WebSocketServerFactory
+    factory = WebSocketServerFactory()
+    factory.protocol = ServerProtocol
+
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(factory, '127.0.0.1', 9000)
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        loop.close()
